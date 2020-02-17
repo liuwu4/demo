5da7d202b0ac1a28ece790d1b65542412e01458d
yj <liuwu4@yeah.net>
2020-02-17 09:50:05 +0800

    feat: 空字段不返回

--- src/main/java/cn/example/demo/dao/Address.java
+++ src/main/java/cn/example/demo/dao/Address.java
@@ -1,5 +1,6 @@
 package cn.example.demo.dao;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Address {
     private Integer addressId;
     private String addressName;
